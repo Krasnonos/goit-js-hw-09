459ed0c1e8db453738a980b3df3790358a007a9a
Krasnonos <andreykrasnonos@icloud.com>
2022-05-13 21:22:07 +0300

    done task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -10,24 +10,33 @@ refs.formEl.addEventListener('submit', onSumbitForm);
 function onSumbitForm(e) {
   e.preventDefault();
 
-  const dalayValue = Number(refs.delayEl.value);
-  const stepValue = Number(refs.stepEl.value);
+  let firstDylayValue = Number(refs.delayEl.value);
+  const nextDelayValue = Number(refs.stepEl.value);
   const amountValue = Number(refs.amountEl.value);
+
+  for (let i = 1; i <= amountValue; i += 1) {
+    createPromise(i, firstDylayValue)
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+
+    firstDylayValue += nextDelayValue;
+  }
 }
 
-// function createPromise(position, delay) {
-//   const shouldResolve = Math.random() > 0.3;
-//   if (shouldResolve) {
-//     // Fulfill
-//   } else {
-//     // Reject
-//   }
-// }
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
 
-// createPromise(2, 1500)
-//   .then(({ position, delay }) => {
-//     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-//   })
-//   .catch(({ position, delay }) => {
-//     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-//   });
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
