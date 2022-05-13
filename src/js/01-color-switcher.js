ea7a2cd343f7cd89e7f2bca6b45fba0f43751cc2
Krasnonos <andreykrasnonos@icloud.com>
2022-05-13 14:16:40 +0300

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
-const a = 5;
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const bodeEl = document.querySelector('body');
+
+startBtn.addEventListener('click', startChangeBgColor);
+stopBtn.addEventListener('click', stopChangeBgColor);
+
+let intervalId = null;
+
+function startChangeBgColor() {
+  intervalId = setInterval(() => {
+    bodeEl.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  startBtn.disabled = true;
+}
+
+function stopChangeBgColor() {
+  clearInterval(intervalId);
+  startBtn.disabled = false;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
