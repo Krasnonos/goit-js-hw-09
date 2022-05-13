ca2e4a6daf588ea80982eca0f060bc6dc69045b4
Krasnonos <andreykrasnonos@icloud.com>
2022-05-13 17:32:22 +0300

    finish addleadingzero

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -50,5 +50,24 @@ function deadLineTimer() {
 
     const currentTime = Date.now();
     timerMS = chosenTime - currentTime;
+    console.log(convertMs(timerMS));
   }, 1000);
+}
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = addLeadingZero(Math.floor(ms / day));
+  const hours = addLeadingZero(Math.floor((ms % day) / hour));
+  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+  const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
+
+  return { days, hours, minutes, seconds };
+}
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
 }
