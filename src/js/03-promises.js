d6ff59c30fd61098eb69dcc32200f72fa6c1ce28
Krasnonos <andreykrasnonos@icloud.com>
2022-05-13 20:46:33 +0300

    start task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,33 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+const refs = {
+  formEl: document.querySelector('.form'),
+  delayEl: document.querySelector('input[name="delay"]'),
+  stepEl: document.querySelector('input[name="step"]'),
+  amountEl: document.querySelector('input[name="amount"]'),
+};
+
+refs.formEl.addEventListener('submit', onSumbitForm);
+
+function onSumbitForm(e) {
+  e.preventDefault();
+
+  const dalayValue = Number(refs.delayEl.value);
+  const stepValue = Number(refs.stepEl.value);
+  const amountValue = Number(refs.amountEl.value);
 }
+
+// function createPromise(position, delay) {
+//   const shouldResolve = Math.random() > 0.3;
+//   if (shouldResolve) {
+//     // Fulfill
+//   } else {
+//     // Reject
+//   }
+// }
+
+// createPromise(2, 1500)
+//   .then(({ position, delay }) => {
+//     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+//   })
+//   .catch(({ position, delay }) => {
+//     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+//   });
